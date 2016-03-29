d057c2cdd65b5ea8fd1b7c6953e01a1f3e7ca56e
Ivan Wang <icw@cmu.edu>
2016-03-28 23:19:06 -0400

    prevent Ctrl-T/S in firefox

--- js/main.js
+++ js/main.js
@@ -8,12 +8,14 @@ React.render(
   document.getElementById('content')
 );
 
-/* Prevent backspace from navigating back in browser.
+/* Prevent backspace from navigating back in browser, or Ctrl-T/Ctrl-S in Firefox.
  * Note: this breaks inputs. See
  * http://stackoverflow.com/questions/1495219/how-can-i-prevent-the-backspace-key-from-navigating-back
  */
 $(document).unbind('keydown').bind('keydown', function(e) {
   if (e.keyCode === 8) {
+    event.preventDefault();
+  } else if (e.ctrlKey && (e.keyCode === 84 || e.keyCode === 85)) {
     event.preventDefault();
   }
 });
