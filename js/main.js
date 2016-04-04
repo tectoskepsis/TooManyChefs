96ad6ca8305d19b3f26eae3f03bca19fea0c256e
Ivan Wang <icw@cmu.edu>
2016-04-04 03:30:02 -0400

    add background music

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var AudioPlayer = require('web-audio-player');
 
 var Game = require('./Game.react.js');
 
+// Render game DOM
 ReactDOM.render(
   <Game />,
   document.getElementById('content')
 );
+
+// Play music
+var audio = AudioPlayer('./audio/Fortaleza.mp3', {loop: true});
+audio.on('load', () => {
+  audio.play();
+  audio.node.connect(audio.context.destination);
+});
+
 
 /* Prevent backspace from navigating back in browser, or Ctrl-T/Ctrl-S in Firefox.
  * Note: this breaks inputs. See
